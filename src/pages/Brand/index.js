471971ce67fd25ba23f50b7e4a65580e02959d3a
chachapara <155712061+chachapara@users.noreply.github.com>
2024-11-14 23:23:55 +0530

    new section add

--- src/pages/Brand/index.js
+++ src/pages/Brand/index.js
@@ -6,18 +6,29 @@ import code from '../../assets/images/18941-Photoroom.png'
 export default function Brand() {
 
     return (
-        <main>
+        <main className='banner-main-wapper'>
             <section className='banner-section-wapper py-5'>
                 <Container>
                     <Row className='align-items-center'>
                         <Col lg={6}>
-                            <h3 className='font-size-min font-black font-w6'>Hi, I am</h3>
-                            <h1 className='font-size-big font-black font-w6'>Sagar Chachapara</h1>
-                            <p className='font-gray font-w7 font-size-small'>Front-end Developer / UI Designer</p>
+                            <h3 className='font-size-min font-black font-w6 font-Raleway'>Hi, I am</h3>
+                            <h1 className='font-size-big font-black font-w6 font-Raleway'>Sagar Chachapara</h1>
+                            <p className='font-gray font-w7 font-size-small font-Raleway'>Front-end Developer / UI Designer</p>
                         </Col>
                         <Col lg={6}>
                             <div className='img-responsive'>
                                 <img src={code} alt="" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+            <section className='banner-about-section py-5'>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <div className='d-flex justify-content-center'>
+                                <p className='text-border-desing font-Montserrat font-w6'>ABOUT ME</p>
                             </div>
                         </Col>
                     </Row>
